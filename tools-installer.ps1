1dd72ad10309cf92df439ac189c79c6589a5944d
AFNAN <209025392+afnan-nex@users.noreply.github.com>
2026-08-11 18:46:18 +0500

    Fixed Scoop

--- tools-installer.ps1
+++ tools-installer.ps1
@@ -103,7 +103,7 @@ function Install-NodeLTS {
 
 function Install-Scoop {
     Start-Process cmd -WindowStyle Minimized -ArgumentList "/k",
-    "echo Installing Scoop... && powershell -NoProfile -ExecutionPolicy Bypass -Command `"irm https://get.scoop.sh > install.ps1; .\install.ps1 -RunAsAdmin; Remove-Item install.ps1`" && echo Adding extras bucket... && scoop bucket add extras && echo. && echo Scoop installation completed. && echo. && echo Press any key to exit . . . && pause >nul && exit"
+    "echo Installing Scoop... && powershell -NoProfile -ExecutionPolicy Bypass -Command `"irm https://get.scoop.sh > install.ps1; .\install.ps1 -RunAsAdmin; Remove-Item install.ps1`" && echo Adding extras bucket... && set PATH=%USERPROFILE%\scoop\shims;%PATH% && scoop bucket add extras && echo. && echo Scoop installation completed. && echo. && echo Press any key to exit . . . && pause >nul && exit"
 }
 
 function Install-Pnpm {
