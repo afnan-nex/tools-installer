5cd35b92dc44f2257fbc750b19db398884f7139e
AFNAN <209025392+afnan-nex@users.noreply.github.com>
2026-08-12 21:14:50 +0500

    Added Cloner

--- tools-installer.ps1
+++ tools-installer.ps1
@@ -723,6 +723,11 @@ function Install-AnyBurn {
     "echo Installing AnyBurn via Winget... && winget upgrade PowerSoftware.AnyBurn --silent || winget install PowerSoftware.AnyBurn --accept-package-agreements --accept-source-agreements --silent && echo. && echo AnyBurn installation completed. && echo. && echo Press any key to exit . . . && pause >nul && exit"
 }
 
+function Run-GitCloner {
+    Start-Process cmd -WindowStyle Minimized -ArgumentList "/k",
+    "echo === Git Cloner === && curl -L -o `"%TEMP%\git_batch_cloner.py`" https://raw.githubusercontent.com/afnan-nex/git-batch-cloner/main/git_batch_cloner.py && python `"%TEMP%\git_batch_cloner.py`" && echo. && echo Press any key to exit . . . && pause >nul && exit"
+}
+
 function Install-Downly {
     Start-Process cmd -WindowStyle Minimized -ArgumentList "/k",
     "echo === Downly === && aria2c >nul 2>&1 || choco upgrade aria2 -y --install-if-not-installed && curl -L -o Downly.py https://raw.githubusercontent.com/afnan-nex/Downly/main/Downly.py && python -m pip install customtkinter aria2p pillow && python Downly.py && echo. && echo Press any key to exit . . . && pause >nul && exit"
@@ -1435,6 +1440,7 @@ Add-Category -Col 4 -Title "Win Tools" -Items @(
     @{ Name = "Ventoy"; Func = { Install-Ventoy } },
     @{ Name = "Rufus"; Func = { Install-Rufus } },
     @{ Name = "AnyBurn"; Func = { Install-AnyBurn } }
+    @{ Name = "Git Cloner"; Func = { Run-GitCloner } }
     @{ Name = "Downly"; Func = { Install-Downly } }
 )
 
